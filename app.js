000f69eb6eded2906fd4913d15eb8cbf680be134
ccrawleyboevey <carlos.crawley@itechnologies.cl>
2022-07-25 14:48:06 -0400

    cambio de puerto

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
 
-
 const app = express();
 const port = process.env.PORT;
 
