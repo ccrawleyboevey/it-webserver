7382e6ff35b9408af1cde9e8cc6d8d44affadfa2
ccrawleyboevey <carlos.crawley@itechnologies.cl>
2022-07-25 13:47:24 -0400

    Commit inicial

---
+++ app.js
@@ -0,0 +1,25 @@
+import 'dotenv/config'
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = path.dirname(__filename);
+
+
+const app = express();
+const port = process.env.PORT;
+
+// Servir contenido estático
+app.use( express.static('public') );
+
+app.get('/hola-mundo', (req, res) => {
+  res.send('Hola mundo en su respectiva ruta');
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/404.html');
+  })
+
+app.listen( port, () => {
+    console.log(`Servidor web escuchando en http://localhost:${ port }`);
+})
